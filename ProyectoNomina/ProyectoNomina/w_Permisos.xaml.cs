2a0440d1f42b89fa299b2f9ff8e2bb6f159f7809
juliortizpy <juliortizpy@gmail.com>
2019-05-21 20:58:20 -0400

    Entity Framework , para conectar BD , se agrega Vista "w_Permisos".

---
+++ ProyectoNomina/ProyectoNomina/w_Permisos.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoNomina
+{
+    /// <summary>
+    /// Lógica de interacción para Permisos.xaml
+    /// </summary>
+    public partial class Permisos : Window
+    {
+
+        //Creamos el objeto 
+        NominaEntities datos;
+        public Permisos()
+        {
+            InitializeComponent();
+            //Se instancia el objeto conexion
+            datos = new NominaEntities();
+        }
+
+        private void CargarPermisos()
+        {
+            try
+            {
+                dgPermisos.ItemsSource = datos.Permisos.ToList();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    
+    }
+}
